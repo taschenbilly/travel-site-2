d94379463ca2b84259371f3f6f8d0ad46527d69b
Billy <taschenbilly@gmx.de>
2020-10-19 01:11:17 +0200

    Header partially ready

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,1 +1,32 @@
-import '../styles/styles.css'
+
+/* 
+class MobileMenu {
+    constructor() {
+        document.querySelector('.site-header__menu-icon').addEventListener('click', function() {
+            console.log('top right icon was clicked');
+        });
+    }
+}
+
+let mobileMenu = new MobileMenu();
+ */
+
+class MobileMenu {
+    constructor() {
+        this.menuIcon = document.querySelector('.site-header__menu-icon');
+        this.menuContent = document.querySelector('.site-header__menu-content');
+        this.siteHeader = document.querySelector('.site-header');
+        this.events();
+    }
+
+    events() {
+        this.menuIcon.addEventListener('click', () => this.toggleTheMenu());
+    }
+
+    toggleTheMenu() {
+        this.menuContent.classList.toggle('site-header__menu-content--is-visible');
+        this.siteHeader.classList.toggle('site-header--is-expanded');
+    }
+}
+
+let mobileMenu = new MobileMenu();
